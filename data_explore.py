1332b6ad4185b16fb74ab7502ccee4e77880f6a9
Andrew Pike <andrewpikegis@gmail.com>
2024-08-05 21:54:21 -0400

    tables are loading and utf 8  problems solved.

--- data_explore.py
+++ data_explore.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
 forwards_path = r"data/forwards.csv"
+goalies_path = r"data/goalies.csv"
+standings_path = r"data/standings.csv"
+managers_path = r"data/Team_Managers.csv"
 
 forwards_tbl = pd.read_csv(forwards_path)
+goalies_tbl = pd.read_csv(goalies_path)
+standings_tbl = pd.read_csv(standings_path)
+managers_tbl = pd.read_csv(managers_path)
+
 
 print(forwards_tbl.head())
+print(goalies_tbl.head())
+print(standings_tbl.head())
+print(managers_tbl.head())
+
