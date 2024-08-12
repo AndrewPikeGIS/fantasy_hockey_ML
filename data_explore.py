55ce2defd1905c478856c0135685016c4e2f9422
Andrew Pike <andrewpikegis@gmail.com>
2024-08-11 22:01:29 -0400

    table cleaning and updates.

--- data_explore.py
+++ data_explore.py
@@ -5,17 +5,21 @@ forwards_path = r"data/forwards.csv"
 goalies_path = r"data/goalies.csv"
 standings_path = r"data/standings.csv"
 managers_path = r"data/Team_Managers.csv"
+waiver_path = r"data/waiver_adds.csv"
 
 forwards_tbl = pd.read_csv(forwards_path)
 goalies_tbl = pd.read_csv(goalies_path)
 standings_tbl = pd.read_csv(standings_path)
 managers_tbl = pd.read_csv(managers_path)
+waiver_tbl = pd.read_csv(waiver_path)
 
 
 manager_join = standings_tbl.merge(managers_tbl, how = "left", on="Team")
 
 manager_join = data_eng.get_goalie_count_per_year(goalies_tbl, manager_join)
 
-print(manager_join.head())
+manager_join = data_eng.add_waiver_count_to_standings(manager_join, waiver_tbl)
 
-print(forwards_tbl.head())
+print(manager_join.head(n=70))
+
+print(manager_join[manager_join["goalie_count"].isna()].head())
--- functions/data_eng_functions.py
+++ functions/data_eng_functions.py
@@ -6,5 +6,10 @@ def get_goalie_count_per_year(goalies_tbl, manager_tbl):
         columns = {"Name":"goalie_count"},
         inplace = True
     )
-    manager_join = pd.merge(manager_tbl, goalies_count, on=["Team", "year"])
+    manager_join = pd.merge(manager_tbl, goalies_count, on=["Team", "year"], how="left")
     return(manager_join)
+
+def add_waiver_count_to_standings(standings, waiver_count):
+    standings_out = standings.merge(waiver_count, on= ["Team", "year"],how = "left")
+    standings_out["waiver_adds"].fillna(0, inplace = True)
+    return(standings_out)
