baf9961f4cbcd3805b6690a992743499eb12abf6
Andrew Pike <andrewpikegis@gmail.com>
2024-08-17 15:19:38 -0400

    added in new shtandings and player stats tables.

--- data_explore.py
+++ data_explore.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import functions.data_eng_functions as data_eng
+import plotly.express as px
 
-forwards_path = r"data/forwards.csv"
-goalies_path = r"data/goalies.csv"
-standings_path = r"data/standings.csv"
-managers_path = r"data/Team_Managers.csv"
-waiver_path = r"data/waiver_adds.csv"
+forwards_path = r"data\forwards.csv"
+goalies_path = r"data\goalies.csv"
+standings_path = r"data\standings.csv"
+managers_path = r"data\Team_Managers.csv"
+waiver_path = r"data\waiver_adds.csv"
+nhl_stand_path = r"data\nhl_standings.csv"
+player_stats_path = r"data\player_stats_full.csv"
 
 forwards_tbl = pd.read_csv(forwards_path)
 goalies_tbl = pd.read_csv(goalies_path)
-standings_tbl = pd.read_csv(standings_path)
+league_standings_tbl = pd.read_csv(standings_path)
 managers_tbl = pd.read_csv(managers_path)
 waiver_tbl = pd.read_csv(waiver_path)
+nhl_standinbgs = pd.read_csv(nhl_stand_path)
+player_stats_path = pd.read_csv(player_stats_path)
 
 
-manager_join = standings_tbl.merge(managers_tbl, how = "left", on="Team")
+manager_join = league_standings_tbl.merge(managers_tbl, how = "left", on="Team")
 
 manager_join = data_eng.get_goalie_count_per_year(goalies_tbl, manager_join)
 
@@ -23,3 +28,7 @@ manager_join = data_eng.add_waiver_count_to_standings(manager_join, waiver_tbl)
 print(manager_join.head(n=70))
 
 print(manager_join[manager_join["goalie_count"].isna()].head())
+
+fig = px.line(nhl_standinbgs, x="year", y="Rk", color="team")
+fig.update_yaxes(autorange = "reversed")
+fig.show()
